cea7e8a9ce18dd23985c8dab521d0b987c078d9c
asifsha <asif-sharif@hotmail.com>
2019-09-28 18:00:41 +0500

    update port number

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 const io = require("socket.io")();
 
-io.on('connection', (client) => {
-    client.on('subscribeToCalculation', (calculation) => {
-      console.log('client is subscribing to get updates of calculator');
-      console.log(calculation);
-      client.broadcast.emit('calculation', calculation);      
-    });
+io.on("connection", client => {
+  client.on("subscribeToCalculation", calculation => {
+    console.log("client is subscribing to get updates of calculator");
+    console.log(calculation);
+    client.broadcast.emit("calculation", calculation);
   });
+});
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 io.listen(port);
 console.log("listening on port ", port);
